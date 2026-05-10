481b03a5071c8987f01f93af978a315a022bfc4c
kavindu_bogahawatte <kavindumalshan2003@gmail.com>
2026-05-10 14:12:05 +0530

    Added SEO optimization

---
+++ kavindu-portfolio/portfolio-frontend/app/robots.ts
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://my-offical-next-js-web-jwe4.vercel.app/sitemap.xml",
+  };
+}
---
+++ kavindu-portfolio/portfolio-frontend/app/sitemap.ts
@@ -0,0 +1,8 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://my-offical-next-js-web-jwe4.vercel.app/",
+      lastModified: new Date(),
+    },
+  ];
+}
